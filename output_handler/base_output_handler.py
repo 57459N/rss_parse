58e01ee947f775169f0a44c0c39452ef19e15297
57459N <57952082+57459N@users.noreply.github.com>
2022-08-13 21:24:06 +0300

    Revert "console_output: added colorized output"

--- output_handler/base_output_handler.py
+++ output_handler/base_output_handler.py
@@ -1,4 +1,7 @@
-class BaseOutputHandler:
+from abc import ABCMeta, abstractmethod
+
+
+class BaseOutputHandler(metaclass=ABCMeta):
     @staticmethod
     def out(data: list):
         raise NotImplementedError
--- output_handler/console_output_handler.py
+++ output_handler/console_output_handler.py
@@ -1,43 +1,34 @@
 import textwrap
 
-import colorama
-from colorama import Fore
-from colorama import init as colorama_init
-
 from output_handler.base_output_handler import BaseOutputHandler
 
 
 class ConsoleOutputHandler(BaseOutputHandler):
     @staticmethod
     def out(data: list, limit: int = 0):
-        colorama_init()
         if limit:
             data = data[:limit]
 
         for article in data:
-            feed_str = Fore.GREEN + 'Feed' + Fore.RESET + ':' + Fore.LIGHTCYAN_EX + f' {article["source"]}'
-            title_str = Fore.GREEN + 'Title' + Fore.RESET + ':' + Fore.RED + f' {article["title"]}'
-            date_str = Fore.GREEN + 'Date' + Fore.RESET + ':' + Fore.LIGHTYELLOW_EX + f' {article["published"]}'
-            link_str = Fore.GREEN + 'Link' + Fore.RESET + ':' + Fore.LIGHTBLUE_EX + f' {article["link"]}'
-            details_str = textwrap.fill(Fore.RESET + f'{article["details"]}', 100)
-
+            feed_str = f'Feed: {article["source"]}'
+            title_str = f'Title: {article["title"]}'
+            date_str = f'Date: {article["published"]}'
+            details_str = f'{article["details"]}'
+            link_str = f'Link: {article["link"]}'
             links = ''
             for index, link in enumerate(article["media"]):
-                links += Fore.MAGENTA + f'[{index + 1}]' + Fore.RESET + ':' + Fore.LIGHTBLUE_EX + f' {link}'
-
-                output = ''
-
-                output += f'''
-{feed_str}
-
-{title_str}
-{date_str}
-{link_str}
-
-{details_str}
-
-{Fore.GREEN + 'Links' + Fore.RESET + ':'}
-{links}
-                        '''
-
-                print(textwrap.dedent(output))
+                links += f'[{index + 1}]: {link}'
+            output = textwrap.dedent(
+                f'''
+                    {feed_str}
+    
+                    {title_str}
+                    {date_str}
+                    {link_str}
+        
+                    {details_str}
+        
+                    Links:
+                    {links}
+                ''')
+            print(output)
